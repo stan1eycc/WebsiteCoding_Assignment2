79c20fe216d50da8156582433d7b0358ed9a8472
stanleycc <stan.chen@live.com>
2016-02-15 13:03:49 +0800

    beta version

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(".google-button").click(function(){
+  $("#linkimg").attr('src',"img/google.png");
+  $("#linkaddr").attr('href',"http://www.google.com");
+});
+
+$(".yahoo-button").click(function(){
+  $("#linkimg").attr('src',"img/yahoo.png");
+  $("#linkaddr").attr('href',"http://www.yahoo.com");
+});
+
+$(".facebook-button").click(function(){
+  $("#linkimg").attr('src',"img/facebook.png");
+  $("#linkaddr").attr('href',"http://www.facebook.com");
+});
